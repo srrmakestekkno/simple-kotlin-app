e3c3138365c0890a9c201996c9bf8564e50af42a
stian røgeberg <srr007@hotmail.com>
2024-10-08 09:51:23 +0200

    Some user input and adding the numbers

--- Main.kt
+++ Main.kt
@@ -1,3 +1,43 @@
 fun main() {
-    println("Hello world")
+    var number: Int? = 0
+    var i = 0
+    var sum: Int = 0
+    while (i < 2){
+        val value = requestUserInput()
+
+        if (value == "Q") {
+            println("Termination")
+            break
+        }
+
+        number = convertToInt(value)
+        if (number != null) {
+            println("You entered the number $number")
+            sum += number
+            i++
+        } else {
+            println("Invalid input. Please enter a valid number or type Q to terminate the application.")
+        }        
+    }
+    println("The sum of the numbers are $sum")
+    
+}
+
+fun requestUserInput() : String? {
+    while (true) {
+
+        print("Enter a number (or press Q to terminate): ")
+        val value = readLine()
+        
+        if (!value.isNullOrEmpty()) {
+            return value
+        } else {
+            println("Input can not be empty. Please try again.")
+        }
+    }
+
+}
+
+fun convertToInt(value: String?) : Int? {
+    return value?.toIntOrNull()
 }
